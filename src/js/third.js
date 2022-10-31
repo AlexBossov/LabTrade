9dea1a5935f62472134bb920a173d62d5c1986b3
Alex <s.bosov2012@yandex.ru>
2022-10-31 15:40:38 +0300

    fix: create base lectures page

--- src/js/third.js
+++ src/js/third.js
@@ -1,8 +1,13 @@
+chooseCategory(0)
+
 
 function chooseCategory(number) {
     let categories = document.getElementsByClassName("category-container__category");
 
     categories[number].style.backgroundColor = "#FFA3A3"
+    let title = document.getElementsByClassName("video-container__title");
+    title[0].innerHTML = categories[number].innerHTML
+    console.log(categories[number])
     for (let i = 0; i < categories.length; i++) {
         if (i !== number)
             categories[i].style.backgroundColor = "#FFF1F1"
